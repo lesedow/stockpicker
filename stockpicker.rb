d74b92d2844399e2e82b849ecf6319021a5da5f3
lesedow <lesedow@gmail.com>
2023-09-28 14:16:06 +0300

    Add comments

--- stockpicker.rb
+++ stockpicker.rb
@@ -17,17 +17,33 @@ end
 def stockpicker (days)
   days_pairs = []
   days_clone = days.clone
-   
+  
+  # Get the smallest and the biggest number in the array
   minmax = get_minmax(days_clone)
 
+
+  #If the biggest number is at the index 0
+  # or the smallest is at the end then slice them off
   if minmax[:min][:index] == days_clone.length - 1
     days_clone.delete_at(minmax[:min][:index])
+    minmax = get_minmax(days_clone)
   elsif minmax[:max][:index] == 0
     days_clone.delete_at(minmax[:max][:index])
+    minmax = get_minmax(days_clone)
   end
 
-  minmax = get_minmax(days_clone)
+  # If the smallest number has an index bigger than the biggst number
+  # then slice the array in half: the first half ranges from the
+  # second smallest number to the biggest number, and the second half
+  # ranges from the smallest number to the second biggest number
 
+  # Get the minmax of both halves and push them into the days pairs array
+  # and sort them by profit 
+
+  # If the smallest doesnt have an index bigger than the biggest number
+  # then just get the min max and put it in the days pairs array
+
+  # Lastly return the index of the smallest and the biggest number
   if minmax[:min][:index] > minmax[:max][:index]
     first_half = days_clone.slice(0, minmax[:max][:index] + 1)
     second_half = days_clone.slice(minmax[:min][:index]..)
